5db518586ab763b8d51de24c117c52c4fc48e8e5
Ben Coyne <bencoyne123@yahoo.co.uk>
2022-04-13 21:43:35 +0100

    hours hand functioning

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const secondsHand = document.querySelector(".second-hand");
 const minutesHand = document.querySelector(".minute-hand");
+const hoursHand = document.querySelector(".hour-hand");
 
 const setDate = () => {
 	const now = new Date();
@@ -11,6 +12,10 @@ const setDate = () => {
 	const minutes = now.getMinutes();
 	const minutesDegrees = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
 	minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+	const hours = now.getHours();
+	const hoursDegrees = (hours / 12) * 360 + (minutes / 60) * 30 + 90;
+	hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
 };
 
 setInterval(setDate, 1000);
